630c58b2c3ec53d343e2fbe7000350682d723821
yzhao <yzhao@fortinet.com>
2022-01-11 06:53:11 -0800

    1

--- deck_test.go
+++ deck_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"testing"
 )
 
@@ -9,7 +10,7 @@ func TestNewDeck(t *testing.T) {
 	d := newDeck()
 	fmt.Println("################# test case 1 begin #################")
 	if len(d) != 52 {
-		t.Errorf("Expected deck length of 16, but got %v", len(d))
+		t.Errorf("Expected deck length of 52, but got %v", len(d))
 	}
 
 	fmt.Println("################# test case 2 begin #################")
@@ -21,4 +22,18 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d) - 1] != "King of Clubs" {
 		t.Errorf("Expected first card of King of Clubs, but got %v", d[len(d) - 1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v \n", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
